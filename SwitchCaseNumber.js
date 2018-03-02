37f6087a5c73ab9a080075b43ef3342a5e9b02e2
nevylmorian <35096942+nevylmorian@users.noreply.github.com>
2018-03-02 16:36:52 +0300

    Add files via upload

---
+++ SwitchCaseNumber.js
@@ -0,0 +1,36 @@
+var dayofTheweek = "Tuesday"
+
+switch(dayofTheweek){
+	case "Monday":
+	console.log("Today is Monday")
+	break;
+
+	case "Tuesday":
+	console.log("Today is Tuesday")
+	break;
+
+	case "Wednesday":
+	console.log("Today is Wednesday")
+	break;
+
+	case "Thursday":
+	console.log("Today is Thursday")
+	break;
+
+	case "Friday":
+	console.log("Today is Friday")
+	break;
+
+	case "Saturday":
+	console.log("Today is Saturday")
+	break;
+
+	case "Sunday":
+	console.log("Today is Sunday")
+	break;
+
+	default:
+	console.log("Error")
+	break;
+
+}
