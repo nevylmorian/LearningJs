de50c19c83041b1e6da36b9ff889058ba6ae8fdc
nevylmorian <35096942+nevylmorian@users.noreply.github.com>
2018-03-02 16:36:04 +0300

    Add files via upload

---
+++ SwitchCase.js
@@ -0,0 +1,16 @@
+var isSunday = false
+
+switch(isSunday){
+	case true:
+	console.log("Today is sunday")
+	break;
+
+	case false:
+	console.log("Today is not sunday")
+	break;
+
+	default:
+	console.log("Error")
+	break;
+
+}
