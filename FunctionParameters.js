a7c1e0736f5544e0d9fb429e6bbc0a4ce5865062
nevylmorian <35096942+nevylmorian@users.noreply.github.com>
2018-03-23 16:09:41 +0300

    added a file

---
+++ FunctionParameters.js
@@ -0,0 +1,20 @@
+function areaOfTriangle(base,height){
+
+	var answer = 0.5 * base * height ;
+	return answer;
+}
+var x = areaOfTriangle(10,18)
+
+
+var y = areaOfTriangle(20,40)
+
+
+var z = areaOfTriangle(10,22)
+
+
+print(x)
+print(y)
+print(z)
+function print( answer){
+	console.log("areaOfTriangle: "+answer)
+}
