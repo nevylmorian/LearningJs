4dddeb55eb8d26a3dca9e05a52a07b41d6a42f63
nevylmorian <35096942+nevylmorian@users.noreply.github.com>
2018-03-23 16:07:05 +0300

    added a file

---
+++ Functions.js
@@ -0,0 +1,57 @@
+// This is a void function
+function add(){
+	var numberOne = 6;
+	var numberTwo = 5;
+	var sum = numberOne + numberTwo;
+	 console.log(sum)
+
+
+}
+//call the function 
+add()
+function multiply(){
+	var numberOne = 4;
+	var numberTwo = 6;
+	var numberThree = 10;
+	var answer = numberOne * numberTwo * numberThree;
+
+	return answer
+}
+var result = multiply()
+console.log(result)
+
+function divide(){
+	var numberOne = 10;
+	var numberTwo = 5;
+	var answer = numberOne / numberTwo
+
+	return answer
+
+}
+var y = divide()
+console.log(y)
+
+function subtract(){
+	var numberOne = 30;
+	var numberTwo = 20;
+	var answer = numberOne - numberTwo
+
+	return answer
+}
+var x = subtract()
+console.log(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
