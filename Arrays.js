1b8f6945d5a207791a39b1212c297bf8ff51df08
nevylmorian <35096942+nevylmorian@users.noreply.github.com>
2018-03-23 16:10:11 +0300

    added a file

---
+++ Arrays.js
@@ -0,0 +1,8 @@
+var student = ["Maurine","Lauren","Ivy","Mary","Fatma","Judith","Irene"]
+var shoeSize = [9, 8, 6, 3, 2, 5]
+// get the lengthof the Array
+console.log(student.length)
+
+for(i=0; i<student.length; i++){
+	console.log(student[i]);
+}
